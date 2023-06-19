8ad8690caf1d3716b479c9a94a134821dea3e103
vikrantcoder7 <137035378+vikrantcoder7@users.noreply.github.com>
2023-06-19 11:32:28 +0000

    Conversionoperations and comparision file added

---
+++ 01_BASICS/03_Conversionoperations.js
@@ -0,0 +1,62 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN ( number type)
+// true => 1; false => 0;
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+
+// In boolean 
+// 1 => true; 0 => false;
+// "" => false;
+// "xyz" => true 
+
+let someNumber = 33;
+
+let stringnumber = String(someNumber);
+
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+
+            /******* OPERATIONS **************/
+
+let value = 3;
+
+let negvalue = -value;
+
+console.log(negvalue);
+
+// 2 ** 2 => denotes power
+
+let str1 = "Hello";
+let str2 = "xyz";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+let gameCounter = 100;
+
+gameCounter++;
+
+console.log(gameCounter);
+
+console.log(2 > 1)
---
+++ 01_BASICS/04_Comparision.js
@@ -0,0 +1,9 @@
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+
+console.log("2" === 2);
