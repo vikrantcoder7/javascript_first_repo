84ae7f006dceeb0dfb7b1beb8029104f0bc528cd
vikrantcoder7 <137035378+vikrantcoder7@users.noreply.github.com>
2023-06-20 07:08:48 +0000

    String and Date file added

--- 01_BASICS/04_Comparision.js
+++ 01_BASICS/04_Comparision.js
@@ -7,3 +7,28 @@ console.log(null >= 0);
 console.log(undefined == 0);
 
 console.log("2" === 2);
+
+//******************** MEMORY ********************/
+
+// STACK -> PRIMITIVE , HEAP -> NON-PRIMITIVE
+
+let myname = "xyz";
+
+let anothername = myname;
+anothername = "abc";
+
+console.log(myname);
+console.log(anothername);
+
+let userOne = {
+     email: "xyz@gmail.com",
+     upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "abc@gmail.com",
+userTwo.upi = "user2@ybl"
+
+console.log(userOne.email);
+console.log(userTwo.email);
---
+++ 01_BASICS/05_Strings.js
@@ -0,0 +1,30 @@
+const name = "vikrant";
+const repoCount = 2;
+
+// console.log(name + repoCount + "value") // old method
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // string interpolation method
+
+const gamename = new String('hiteshhc');
+
+// console.log(gamename[0]);
+
+// console.log(gamename.__proto__);         // getting a type 
+
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('t'));
+
+const newString = gamename.substring(0, 4);
+
+console.log(newString);
+
+const anotherstring = gamename.slice(-2, 5);
+
+console.log(anotherstring);
+
+
+
+
+
+
+
