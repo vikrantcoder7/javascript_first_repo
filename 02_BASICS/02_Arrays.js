766fab96c44fb82cad9032f484cf4844f669eff9
vikrantcoder7 <137035378+vikrantcoder7@users.noreply.github.com>
2023-06-21 04:50:00 +0000

    Array 2 - basics of array

---
+++ 02_BASICS/02_Arrays.js
@@ -0,0 +1,35 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "Batman"]
+
+//marvel_heros.push(dc_heros);
+
+// console.log(marvel_heros);
+
+// const all_heros = marvel_heros.concat(dc_heros);
+
+// console.log(all_heros);
+
+// const all_new_heros = [...marvel_heros, ...dc_heros]  // ... = spread operation -> seperates the elements in a array
+
+// console.log(all_new_heros);
+
+// const another_arr = [1, 2, 3, [4, 5, 6], 7, [8, 9, [10, 11]]]
+
+// console.log(another_arr);
+
+// const flatarray = another_arr.flat(Infinity);
+
+// console.log(flatarray);
+
+// console.log(Array.isArray("Vikrant")); // identify a particular input is array or not
+// console.log(Array.from("Vikrant"));
+
+//console.log(Array.from({name: "Vikrant"})); // Prints empty array
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+
+
