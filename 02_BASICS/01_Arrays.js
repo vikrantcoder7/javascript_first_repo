5d84e9eab7e2e8b895bbb835b0946cdd6a982962
vikrantcoder7 <137035378+vikrantcoder7@users.noreply.github.com>
2023-06-21 04:16:37 +0000

    Array file - slice and splice operations

--- 02_BASICS/01_Arrays.js
+++ 02_BASICS/01_Arrays.js
@@ -7,16 +7,37 @@ const arr3 = arr1.concat(arr2);
 
 const myarr3 = new Array(1, 2, 3, 4, 5);
 
-console.log( `The array after concatination is ${arr3} number of characters`);
+// console.log( `The array after concatination is ${arr3} number of characters`);
 
 // ARRAY METHODS
 
-arr1.push(8);
-console.log(arr1);
+// arr1.push(8);
+// console.log(arr1);
 
-console.log(myarr3);
+// console.log(myarr3);
 
-const cell = ['a', 'b', 'v'];
+// myarr3.unshift(9);
+// console.log(myarr3);
+// myarr3.shift();
+// console.log(myarr3);
 
-console.log(cell);
+// const newarrjoin =  myarr3.join();
+
+// console.log(myarr3);
+// console.log(newarrjoin);
+// console.log(typeof newarrjoin);
+
+// console.log("A " + myarr3);
+
+// const newsclicearr = myarr3.slice(1, 3);
+
+// console.log(newsclicearr);
+
+// console.log("B " + myarr3);
+
+// const newsplicearr = myarr3.splice(1,3);
+
+// console.log(newsplicearr)
+// console.log("C "+ myarr3);
+
 
