48896555879dbabdac770d15f7243e2f525beaa3
vikrantcoder7 <137035378+vikrantcoder7@users.noreply.github.com>
2023-06-26 04:30:50 +0000

    Arrow Function - implicit , explicit return

--- 03_BASICS/03_Arrow.js
+++ 03_BASICS/03_Arrow.js
@@ -15,8 +15,49 @@
 
 // console.log(this);
 
-function chai(){
-      console.log(this);
-}
+// function chai(){
+//       let username = "hitesh"
+//       console.log(this.username);
+// }
 
-chai();
+// const chai = function() {
+//        let username= "vikrant"
+//        console.log(this.username);
+// }
+
+// chai();
+
+
+           /************   ARROW FUNCTION **********/
+
+// const chai = function() {
+     
+//        let username = "hitesh"
+//        console.log(this.username);
+
+// }
+
+          /************   ARROW FUNCTION **********/
+
+// const chai = () => {
+
+//     let username = "hitesh"                              // function definition
+//     console.log(this);
+       
+// }          
+
+// const addtwo = (num1, num2) => {                         // Basic Arrow functions with parameters
+//        return num1 + num2;                               // explicit return
+// }
+
+const addtwo = (num1 , num2) => num1+num2;                   // implicit return - method 1
+
+const addtwo2 = (num1 , num2) => (num1+num2);                // implicit return - method 2
+
+const objret = () => ({username : "sam"});                    // implicit object return
+
+console.log(objret());
+
+
+
+
